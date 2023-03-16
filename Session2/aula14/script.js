13c73b5c655eb59db77da2592e26f60011580fda
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-16 20:34:00 -0300

    add..

---
+++ Session2/aula14/script.js
@@ -0,0 +1,9 @@
+/*
+Nomeclaturas de variáveis no JavaScript
+*/
+
+// Taxa de Juros
+let tj = 2.9;
+let TaxaJuros = 2.9; // Faz mais sentido para uma futura manutenção do código.
+
+// Palavras reservadas da linguagem e não podem ser usadas.
