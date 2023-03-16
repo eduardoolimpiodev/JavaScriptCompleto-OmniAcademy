9dd40c67f903a98eae340bb4497538fc327eacc1
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-16 17:54:55 -0300

    Tipo de Dados JS

---
+++ aula10/tipoDeDados.js
@@ -0,0 +1,36 @@
+/*
+Primitivos:
+Tipo de dados no JavaScript
+String
+Number
+BigInt
+Boolean
+Undefined
+Symbol
+
+Primitivo Especial:
+Null
+
+Complexos:
+Object
+Function
+
+String: Texto puro, conjunto de caracteres.
+"Eduardo Olimpio"
+'Eduardo Olimpio'
+
+Number: Representa os números, quantidade, valores, preços, etc...
+Special Numbers: Números de ponto flutuantes ou inteiros.
+
+1222
+1.256
+
+Boolean: True/False
+
+Symbol: É um valor excliso e imutável
+
+BigInt: Representa números inteiros maiores que 2^53
+
+Undefined: Valor não atribuído.
+*/
+
