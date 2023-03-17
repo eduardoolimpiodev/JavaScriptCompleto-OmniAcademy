e60711600eb109699db065e17b173f31592de5ab
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-17 00:09:59 -0300

    Tipagem Dinamica

---
+++ Session2/aula16/tipagemDinamica.js
@@ -0,0 +1,10 @@
+
+let nome = 'Eduardo';
+let sobrenome = 'Olimpio';
+let idade = 36;
+const numeroCliente = 2521;
+let valorEmprestimo = 150000;
+let taxaDeJuros = 10;
+let bomPagador = true;
+
+console.log("Boa tarde Sr. " + nome + " " + sobrenome + ". Sua idade é " + idade + " anos.");
