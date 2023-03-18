a9580316f3711600487b88fb8bad09cb57ff9d29
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-03-18 14:39:39 -0300

    Add: oeprador ternário

---
+++ Session4/aula32/operadorTernario.js
@@ -0,0 +1,20 @@
+/* 
+
+Operadores ternários.
+(condicao) ? operando1 : operando2;
+
+*/
+let bomPagador = true;
+let nome = 'Eduardo';
+
+if(bomPagador) {
+    console.log("O cliente "+ nome + " é um bom pagador!");
+} else {
+    console.log("O cliente "+ nome + " não é um bom pagador!");
+}
+
+//Com operador ternário:
+
+console.log("O cliente " + nome + (bomPagador ? " é bom pagador." : " não é bom pagador."));
+
+console.log(bomPagador ? "O cliente "+ nome + " é um bom pagador!" : "O cliente "+ nome + " não é um bom pagador!");
